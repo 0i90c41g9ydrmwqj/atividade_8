f2b29fe2617e8d2fa1654401a2dc16f249325441
Estudante matricula 123.879 <123.879@estudante_unigran.com>
2023-03-11 23:32:25 +0000

    O programa calcula o IMC

--- imc.py
+++ imc.py
@@ -1,2 +1,4 @@
-massa = input("Forneça a massa, em quilogramas: ")
-altura = input("Forneça a altura, em metros: ")
+massa = float(input("Forneça a massa, em quilogramas: "))
+altura = float(input("Forneça a altura, em metros: "))
+imc = massa/altura**2
+print("O IMC é : {}".format(imc))
